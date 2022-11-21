804f44a6dfaf076031b590d2c2027affabbdffa9
Andrej Streicher <andrejstreicher1@gmail.com>
2022-11-21 15:35:34 +0100

    MenuStart

--- TradingBotCS/MainConsole/MainInput.cs
+++ TradingBotCS/MainConsole/MainInput.cs
@@ -6,7 +6,8 @@
 
         private static async Task Main(string[] args)
         {
-            WelcomeOptions();
+            // WelcomeOptions();
+            TestMethod();
             var firstInput = Console.ReadLine();
             switch (firstInput)
             {
@@ -16,7 +17,7 @@
                     string inputTwo = Console.ReadLine();
                     if (inputTwo == "1")
                     {
-                        string responseJson = await GetFromApi.HttpRequestInfo(_apiKey,"price");
+                        string responseJson = await GetFromApi.HttpRequestInfo(_apiKey, "price");
                     }
                     else if (inputTwo == "2")
                     {
@@ -24,7 +25,11 @@
                     }
                     else if (inputTwo == "3")
                     {
-                        string responseJson = await GetFromApi.HttpRequestInfo(_apiKey,"quote");
+                        string responseJson = await GetFromApi.HttpRequestInfo(_apiKey, "quote");
+                    }
+                    else if (inputTwo == "4")
+                    {
+                        WelcomeOptions();
                     }
 
                     break;
@@ -35,20 +40,45 @@
             }
         }
 
-        private static void WelcomeOptions()
+        private static async void WelcomeOptions()
         {
             Console.WriteLine("Welcome! What would you like to do ?");
             Console.WriteLine("1.Get ticker info (current price, time-series, quote...)");
             Console.WriteLine("2.Get technical indicator");
             Console.WriteLine("3.Execute real-time strategy");
             Console.WriteLine("4.Backtest an existing strategy");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    GetTickerOptions();
+                    break;
+            }
         }
 
-        private static void GetTickerOptions()
+        private static async void GetTickerOptions()
         {
             Console.WriteLine("1. Current price");
             Console.WriteLine("2. Time-series");
             Console.WriteLine("3. Quote");
+            Console.WriteLine("4. Go back");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    string responseJson = await GetFromApi.HttpRequestInfo(_apiKey, "price");
+                    break;
+            }
+        }
+
+        private static void TestMethod()
+        {
+            string prompt = "Welcome! What would you like to do ?";
+            string[] options =
+            {
+                "Get ticker info (current price, time-series, quote...)", "Get technical indicator",
+                "Execute real-time strategy", "Backtest an existing strategy"
+            };
+            Menu mainMenu = new Menu(prompt, options);
+            int selectedItem = mainMenu.Run();
         }
     }
 }
