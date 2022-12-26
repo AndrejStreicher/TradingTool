92d9b7f4f1fa38bfc9d557b07eff62a293d5a25e
Andrej Streicher <andrejstreicher1@gmail.com>
2022-12-26 23:21:50 +0100

    Deleted unnecessary Root class

--- MainConsole/Deserialization Classes/MultiplePricesClass.cs
+++ MainConsole/Deserialization Classes/MultiplePricesClass.cs
@@ -2,11 +2,6 @@
 
 public class MultiplePricesClass
 {
-    public class Root
-    {
-        public Dictionary<string, Price> Prices { get; set; }
-    }
-
     public class Price
     {
         public string price { get; set; }
